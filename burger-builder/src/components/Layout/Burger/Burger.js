4e05ccdfcd0d59f0fa0792ed30d6586c389cc771
Karanaggarwal96 <karanaggarwal30@gmail.com>
2020-02-14 23:57:37 -0500

    Minor Change

--- burger-builder/src/components/Layout/Burger/Burger.js
+++ burger-builder/src/components/Layout/Burger/Burger.js
@@ -3,12 +3,18 @@ import  './Burger.css'
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
 
 const Burger=(props)=>{
-    const transformedIngriedent=Object.keys(props.ingredients).map(igKey=>{
+    //let transformedIngriedent=Object.keys(props.ingredients).map(igKey=>{
         return [...Array(props.ingredients[igKey])].map((_,i)=>{
 
             return <BurgerIngredient key={igKey +i } type={igKey}/>
         })
-    })
+    }).reduce((arr,el)=>{
+        return arr.concat(el);
+    },[])
+    if(transformedIngriedent.length===0)
+    {
+        transformedIngriedent=<p>Please Start Adding Some Ingredients</p>
+    }
     return(
         <div className='Burger'>
         <BurgerIngredient type='bread-top'/>
